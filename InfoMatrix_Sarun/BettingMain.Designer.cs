5115d8bb3962e77a3bc9197beef5c640b025f5f0
SarunMK <sarun.m.k@gmail.com>
2016-09-29 15:47:25 +1000

    Add project files.

---
+++ InfoMatrix_Sarun/BettingMain.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace InfoMatrix_Sarun
+{
+    partial class BettingMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BettingMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 728);
+            this.Name = "BettingMain";
+            this.Text = "Betting Information";
+            this.Load += new System.EventHandler(this.BettingMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
